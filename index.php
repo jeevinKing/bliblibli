726dd50bf1b11828938e18f098be7ac39971a269
jeevinKing <jeevin@126.com>
2018-10-08 23:49:10 +0800

    增加了爬虫图片

--- index.php
+++ index.php
@@ -1,89 +1,41 @@
-<?php 
+<?php
 error_reporting(E_ALL ^ E_NOTICE);
 require './vendor/autoload.php';
 
 use QL\QueryList;
 
-function init() {
-	$url = "http://www.szwj72.cn/t/shuqingsanwen/";
-	
+$url = "http://www.wenzhangba.com/qingganwenzhang";
+$rule = [
+    'title' => ['.subBox strong a','text'],
+    'url'   => ['.subBox strong a','href']
+];
 
-	$rule = [
-		'title' => ['.excerpt header h2 a','text'],
-		'url'   => ['.excerpt header h2 a','href'],
-		'desc'   => ['.excerpt p.note','text']
-	];
-	$list_data = craw_data($url,$rule);
-	
-	$reg = [
-	 'content' => array('.article-content','html')
-	];
-	foreach ($list_data as $key => $value) {
-		// $detail = craw_data($value['url'],['content' =>['.article-content','html']]);
-		
-		$data = QueryList::Query($value['url'],$reg,'.content')->getData(function($item){
-		    //图片本地化
-		    $item['content'] = QueryList::run('DImage',[
-		            'content' => $item['content'],
-		            'image_path' => '/img/'.date('Y-m-d'),
-		            'www_root' => dirname(__FILE__)
-		        ]);
-		    return $item;
-		});
+$list = craw_data($url,$rule);
+// dd($list);
+foreach ($list as $key => $value) {
+    $html = $value['url'];
+    $detail = QueryList::Query($html,array(
+            'content' => array('.a_detail','html','a')
+        ))->getData(function($item){
+            $item['content'] = QueryList::run('DImage',[
+                'content' => $item['content'],
+                'image_path' => 'images/'.date('Y-m-d'),
+                'www_root' => dirname(__FILE__)
+            ]);
+            return $item;
+        });
 
-
-		// $data['title'] = $list_data[$key]['title'];
-		// $data['url'] = $list_data[$key]['url'];
-		// $data['desc'] = $list_data[$key]['desc'];
-		print_r("<pre>");
-		print_r($data);
-	}
-	
+    $data['title'] = $list[$key]['title'];
+    $data['content'] = $detail[0]['content'];
+    echo "<pre>";
+    print_r($data);
 }
 
-	function craw_data($url,$rule) {
-		$data = QueryList::Query($url,$rule,'','UTF-8','GB2312')->data;
-		return $data;
-	}
-	$url = "http://web.jobbole.com/category/career/";
-	$rule = [
-		'title' => ['.archive-title','text'],
-		'url'   => ['.archive-title','href']
-	];
-	$list_data = craw_data($url,$rule);
-	print_r("<pre>");
-	print_r($data);
-	
-	// foreach ($list_data as $key => $value) {
-	// 	$reg = [
-	// 		'content' => ['.article-content','html']
-	// 	];
-	// 	$data = QueryList::Query($value['url'],$reg,'.content')->getData(function($item){
-	// 	    //图片本地化
-	// 	    $item['content'] = QueryList::run('DImage',[
-	// 	            'content' => $item['content'],
-	// 	            'image_path' => '/images/'.date('Y-m-d'),
-	// 	            'www_root' => dirname(__FILE__)
-	// 	        ]);
-	// 	    return $item;
-	// 	});
-	// 	print_r("<pre>");
-	// 	print_r($data);
-	// }
+function craw_data($url,$rule) {
+    ##解决乱码问题
+    // $data = QueryList::Query($url,$rule,'','UTF-8','GB2312')->data;
+    $data = QueryList::Query($url,$rule)->data;
+    return $data;
+}
 
-	// $url = "http://www.goodmood.cc/life/23551.html";
-	// $rule = [
-	// 	'content' => ['.article-content','html']
-	// ];
-	// $data = QueryList::Query($url,$rule,'.content')->getData(function($item){
-	//     //图片本地化
-	//     $item['content'] = QueryList::run('DImage',[
-	//             'content' => $item['content'],
-	//             'image_path' => '/images/'.date('Y-m-d'),
-	//             'www_root' => dirname(__FILE__)
-	//         ]);
-	//     return $item;
-	// });
-
-// init();
 
